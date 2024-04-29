e25bf785f26b420c76141fe16f1cc76b1c6a3a5d
Hanz <hanz@godot.id>
2024-04-29 15:15:36 +0700

    fix: Fixed `process.cwd` for deployed project feat: Added `decompress-zip` feat: Added a patch for `decompress-zip` feat: zlib compression level set to 9

--- boilerplate/index.js
+++ boilerplate/index.js
@@ -1,14 +1,45 @@
 const fs = require('fs');
-const unzipper = require('unzipper');
+const DecompressZip = require('decompress-zip');
+const { spawn } = require('child_process');
 
 const dir = process.cwd();
 
-const appUUID = crypto.randomUUID();
+const extractor = new DecompressZip(__dirname + '/app.zip');
+const appUUID = fs.readFileSync(__dirname + '/app.zip.sha256', 'utf8').toString();
+const appDir = dir + '/' + appUUID;
 
-const extractor = unzipper.Extract({ path: dir + `/${appUUID}/` });
-const stream = fs.createReadStream(__dirname + '/app.zip');
-stream.pipe(extractor);
-extractor.on('close', () => {
-    console.log(fs.readdirSync(dir + '/' + appUUID));
-    fs.rmdirSync(dir + '/' + appUUID, { recursive: true });
+function startApp() {
+    process.chdir(appDir);
+    try {
+        const package = JSON.parse(fs.readFileSync(appDir + '/package.json'));
+        const child = spawn(
+            process.argv[0], [appDir + '/' + package.main, process.argv.slice(2)],
+            {
+                cwd: appDir, shell: true,
+                stdio: 'inherit', detached: false
+            });
+    } catch (e) {
+        console.error(e);
+    } finally {
+        if (process.env.REMOVE_APP_DIR == '1') {
+            fs.rmSync(appDir, { recursive: true });
+        }
+    }
+}
+
+extractor.on('extract', () => {
+    startApp();
 });
+
+extractor.on('error', (err) => {
+    console.error(err);
+})
+
+if (fs.existsSync(appDir + '/package.json')) {
+    // Don't extract if the app is already extracted
+    startApp();
+} else {
+    extractor.extract({
+        path: appDir + '/'
+    });
+}
--- index.js
+++ index.js
@@ -3,23 +3,50 @@
 const pkg = require('@yao-pkg/pkg');
 const archiver = require('archiver');
 const fs = require('fs');
+const path = require('path');
+const crypto = require('crypto');
 
 const argv = process.argv.slice(2);
 
 const archive = archiver('zip', {
     zlib: {
-        level: 0
+        level: 9
     }
 });
-archive.pipe(fs.createWriteStream('boilerplate/app.zip'));
-archive.directory(argv[0], false);
-archive.finalize();
+archive.pipe(fs.createWriteStream(__dirname + '/boilerplate/app.zip'));
+// archive.glob('**/*', { cwd: path.resolve(process.cwd(), argv[0]), dot: true });
+archive.directory(path.resolve(process.cwd(), argv[0]), false);
 
 archive.on('close', async () => {
-    await pkg.exec([
-        'boilerplate/index.js',
-        '--targets', 'node20-windows-x64',
-        '--config', 'boilerplate/package.json',
-        '--output', argv[1]
-    ])
+    console.log("[$] Project files compressed");
+    const hash = crypto.createHash('sha256');
+    const rs = fs.createReadStream(__dirname + '/boilerplate/app.zip');
+
+    console.log("[*] Calculating project SHA256 hash...")
+    rs.on('data', (data) => { hash.update(data); });
+    rs.on('end', async () => {
+        console.log("[*] Writing hash to file...")
+        fs.writeFileSync(__dirname + '/boilerplate/app.zip.sha256', hash.digest('hex'));
+        console.log("[$] Hash written to file");
+
+        console.log("[*] Packaging project files...");
+        await pkg.exec([
+            __dirname + '/boilerplate/index.js',
+            '--targets', 'node20-windows-x64',
+            '--config', __dirname + '/boilerplate/package.json',
+            '--output', path.resolve(argv[1])
+        ]);
+        console.log("[$] Project files packaged to", path.resolve(argv[1]));
+    });
 });
+
+archive.on('warning', (err) => {
+    console.error(err);
+});
+
+archive.on('error', (err) => {
+    console.error(err);
+});
+
+console.log("[*] Compressing project files...");
+archive.finalize();
